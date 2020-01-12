3cd649d5242bca920379099d60533e795e76df79
lihanjin <1213219813@qq.com>
2020-01-13 02:07:23 +0800

    主页用户界面初始化

---
+++ pages/index/config.js
@@ -0,0 +1,39 @@
+export default {
+  pro1Name: '所有商铺',
+  pro1: [{
+    text: '杭州',
+    id: 1
+  }, {
+    text: '温州',
+    id: 2
+  }, {
+    text: '宁波',
+    id: 3,
+    disabled: true
+  }, {
+    text: '义乌',
+    id: 4
+  }],
+  pro2Name: '第一层',
+  pro2: [{
+    text: '南京',
+    id: 5
+  }, {
+    text: '无锡',
+    id: 6
+  }, {
+    text: '徐州',
+    id: 7
+  }, {
+    text: '苏州',
+    id: 8
+  }],
+  pro3Name: '第二层',
+  pro3: [{
+    text: '泉州',
+    id: 9
+  }, {
+    text: '第三层',
+    id: 10
+  }]
+};
